777e7c55f2bae41316c76a3eb22b236d8e670225
Nui-backend-intern <100454921+Mrpk2109@users.noreply.github.com>
2022-04-26 15:19:04 +0700

    for peeA

--- sever.ts
+++ sever.ts
@@ -1,9 +1,15 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const data = []
 
-app.get('/',(req,res)=>{
-    res.send('Hello World')
+app.get('/',(req ,res)=>{
+    res.json(data)
+})
+
+app.post('/',(req,res)=>{
+    data.push(res.body)
+    res.json(data)
 })
 
 app.listen(port, () => {
