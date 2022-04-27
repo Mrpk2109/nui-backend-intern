23c77db27e7843851ab90dcc45f4a5f29dffde51
Nui-backend-intern <100454921+Mrpk2109@users.noreply.github.com>
2022-04-27 10:43:13 +0700

    Restful success

--- sever.ts
+++ sever.ts
@@ -8,20 +8,26 @@ app.get('/',(req ,res)=>{
     res.json(data)
 })
 
-
+app.use(express.json());
 app.post('/',(req,res)=>{
     const user = req.body
     data.push(user)
     res.json(data)
 })
 
-app.put('/:id',(req,res)=>{
-    data.findByIdAndUpdate
-    req.params.id,
-    {
-        $set: req.body
-    }
+
+
+app.put('/:id', (req, res) => {
+    const updateIndex = data.findIndex(data => data.id === req.params.id)
+    res.json(Object.assign(data[updateIndex], req.body))
+  })
+
+app.delete('/:id', (req, res) => {
+    const deleteIndex = data.findIndex(data => data.id === req.params.id)
+    data.splice(deleteIndex, 1)
 })
+
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
   })
