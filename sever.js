2e8b9cb931d73b93f21712defca41d1e9a34983f
Nui-backend-intern <100454921+Mrpk2109@users.noreply.github.com>
2022-04-28 14:38:49 +0700

    one

--- sever.js
+++ sever.js
@@ -1,38 +1,32 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const data = require('./data.json')
-const querystring = require('querystring')
-app.use(express.json());
-
-
-
-app.get('/',(req ,res)=>{
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const port = 3000;
+const data = require('./data.json');
+const querystring = require('querystring');
+app.use(express_1.default.json());
+app.get('/', (req, res) => {
     console.log(JSON.stringify(req.headers));
     console.log('id: ' + req.query.id);
-    res.json(data)
-}); 
-
-
-app.post('/',(req,res)=>{
-    const user = req.body
-    data.push(user)
-    res.json(data)
-})
-
-
-
+    res.json(data);
+});
+app.post('/', (req, res) => {
+    const user = req.body;
+    data.push(user);
+    res.json(data);
+});
 app.put('/:id', (req, res) => {
-    const updateIndex = data.findIndex(data => data.id === req.params.id)
-    res.json(Object.assign(data[updateIndex], req.body))
-  })
-
+    const updateIndex = data.findIndex((data) => data.id === req.params.id);
+    res.json(Object.assign(data[updateIndex], req.body));
+});
 app.delete('/:id', (req, res) => {
-    const deleteIndex = data.findIndex(data => data.id === req.params.id)
-    data.splice(deleteIndex, 1)
-})
-
-
+    const deleteIndex = data.findIndex((data) => data.id === req.params.id);
+    data.splice(deleteIndex, 1);
+});
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
+    console.log(`Example app listening on port ${port}`);
+});
---
+++ sever.ts
@@ -0,0 +1,38 @@
+import express,{Request , Response} from 'express'
+const app = express()
+const port = 3000
+const data = require('./data.json')
+const querystring = require('querystring')
+app.use(express.json());
+
+
+
+app.get('/',(req:Request ,res:Response)=>{
+    console.log(JSON.stringify(req.headers));
+    console.log('id: ' + req.query.id);
+    res.json(data)
+}); 
+
+
+app.post('/',(req:Request ,res:Response)=>{
+    const user = req.body
+    data.push(user)
+    res.json(data)
+})
+
+
+
+app.put('/:id', (req:Request ,res:Response) => {
+    const updateIndex = data.findIndex((data: { id: string }) => data.id === req.params.id)
+    res.json(Object.assign(data[updateIndex], req.body))
+  })
+
+app.delete('/:id', (req:Request ,res:Response) => {
+    const deleteIndex = data.findIndex((data: { id: string }) => data.id === req.params.id)
+    data.splice(deleteIndex, 1)
+})
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
