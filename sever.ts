2cb3e894c7e58b0d529348a83a8c8b43a7c31d14
Nui-backend-intern <100454921+Mrpk2109@users.noreply.github.com>
2022-04-29 11:14:01 +0700

    status code

--- sever.ts
+++ sever.ts
@@ -1,19 +1,23 @@
 import express,{Request , Response} from 'express'
 import { query } from 'express'
-import data,{id,name,description,image} from ("../nui-backend-intern");
 
 const app = express()
 const port = 3000
-//const data = require('./data.json')
-//const querystring = require('querystring')
 app.use(express.json());
 
-
+const data = [
+    {
+        "id" : "1",
+        "name" : "Panuphong",
+        "description" : "I'm Intern",
+        "image" : "isgsdf"
+    }]
 
 app.get('/',(req:Request ,res:Response)=>{
     console.log(JSON.stringify(req.headers));
     console.log('id: ' + req.query.id);
     res.json(data)
+    res.status(200).send("Success")
 }); 
 
 
@@ -21,18 +25,20 @@ app.post('/',(req:Request ,res:Response)=>{
     const user = req.body
     data.push(user)
     res.json(data)
+    res.status(201).send("Create new respone");
 })
-
-
 
 app.put('/:id', (req:Request ,res:Response) => {
     const updateIndex = data.findIndex((data: { id: string }) => data.id === req.params.id)
     res.json(Object.assign(data[updateIndex], req.body))
+    res.status(205).send("Input and update success");
   })
+
 
 app.delete('/:id', (req:Request ,res:Response) => {
     const deleteIndex = data.findIndex((data: { id: string }) => data.id === req.params.id)
     data.splice(deleteIndex, 1)
+    res.status(204).send("Delete success");
 })
 
 
